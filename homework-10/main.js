adc5773bc9d5612f1ba11a454646babca3262c2c
Karina1213 <karinaa121213@gmail.com>
2019-12-17 22:01:01 +0200

    fix

--- homework-10/main.js
+++ homework-10/main.js
@@ -1,18 +1,31 @@
-
 async function asynkFunk() {
     const url = `https://api.github.com/gists/public`;
     try {
-        console.log('start.........')
-        const response = await fetch(url)
-        const data = await response.json()
-        console.log('Data:', data)
+        const response = await fetch(url);
+        const data = await response.json();
+        let  user = 'login: ' + data[0].owner.login +" id:" + data[0].node_id;
+        alert(user);
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 }
-asynkFunk()
+
+ function but() {
+     let button = document.getElementById('button');
+     button.addEventListener('click', asynkFunk)
+ }
+but();
 
 
+
+
+
+
+
+// await load-users.onclick = () => {
+//     for ( let key in data) {
+//         console.log(key + data[key]);   // data[0].owner.login + data[0].node_id
+//     }
 
 
 
